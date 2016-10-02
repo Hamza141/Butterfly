3b5428fc9abd82dddb989477997b9c6b5ae5be37
AjDeF <ajdefano@gmail.com>
2016-10-02 15:56:09 -0400

    added google service plugin to app and project build.gradle

--- Client/app/src/main/java/cs307/butterfly/LoginActivity.java
+++ Client/app/src/main/java/cs307/butterfly/LoginActivity.java
@@ -64,8 +64,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        
+
+
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
