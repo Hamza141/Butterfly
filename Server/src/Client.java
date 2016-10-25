3098628c311cedc2f02e7088801e957636f91819
trankq <trankq@purdue.edu>
2016-10-25 18:28:33 -0400

    add user and community

--- Server/src/Client.java
+++ Server/src/Client.java
@@ -8,7 +8,7 @@ import org.json.simple.JSONObject;
 
 public class Client {
     public static void main(String[] args) {
-        String serverName = "10.186.80.161";
+        String serverName = "128.211.225.79";
         int port = 60660;
         try {
             System.out.println("Connecting to " + serverName + " on port " + port);
@@ -17,11 +17,18 @@ public class Client {
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
             JSONObject obj = new JSONObject();
-            obj.put("function", "addUser");
+            /*obj.put("function", "addUser");
             obj.put("firstName", "Khanh");
             obj.put("lastName", "Tran");
             obj.put("GoogleID", "Google1");
-            obj.put("dateCreated", "2016-10-09");
+            obj.put("dateCreated", "2016-10-09");*/
+            obj.put("function", "addCommunity");
+            obj.put("category", "sports");
+            obj.put("subCategory", "hockey");
+            obj.put("name", "Red Wings");
+            obj.put("description", "Best Original 6");
+            obj.put("dateCreated", "2016-10-23");
+            obj.put("private", "0");
             out.writeUTF(obj.toString());
             InputStream inFromServer = client.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -18,30 +18,28 @@ public class Server extends Thread {
     static final private String DB_URL = "jdbc:mysql://localhost/Butterfly";
     static final private String USER = "root";
     static final private String PASS = "Ghost999";
-    static private Connection conn = null;
-    static private Statement stmt = null;
-    static private ResultSet rs;
-    static private String sql;
+    private Connection conn = null;
+    private Statement stmt = null;
+    private ResultSet rs;
+    private String queryCheck;
+    private PreparedStatement ps;
     static private long id;
+    static private DataOutputStream out;
 
     private Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
     }
 
     public void run() {
-
+        try {
+            URL myip = new URL("http://checkip.amazonaws.com");
+            BufferedReader buffIn = new BufferedReader(new InputStreamReader(myip.openStream()));
+            String ip = buffIn.readLine();
+            System.out.println(ip);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         while (true) {
-            try {
-                URL whatismyip;
-                whatismyip = new URL("http://checkip.amazonaws.com");
-                BufferedReader in;
-                in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
-                String ip = in.readLine();
-                System.out.println(ip);
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-
             System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
             try {
                 Class.forName("com.mysql.jdbc.Driver");
@@ -51,24 +49,24 @@ public class Server extends Thread {
                 Socket server = serverSocket.accept();
                 System.out.println("Just connected to " + server.getRemoteSocketAddress());
                 JSONParser parser = new JSONParser();
-                DataInputStream is = new DataInputStream(server.getInputStream());
-                Object obj = parser.parse(is.readUTF());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+                out = new DataOutputStream(server.getOutputStream());
+                Object obj = parser.parse(in.readUTF());
                 JSONObject obj2 = (JSONObject) obj;
                 String function = (String) obj2.get("function");
                 if (function.equals("addUser")) {
                     addUser(obj2);
                 } else if (function.equals("addCommunity")) {
-                    String name = (String) obj2.get("communityName");
-                    sql = "insert into Communities (name) values('" + name + "')";
-                    stmt.execute(sql);
+                    addCommunity(obj2);
                 } else if (function.equals("addEvent")) {
                     String eventName = (String) obj2.get("eventName");
                     String eventTime = (String) obj2.get("eventTime");
                     String desc = (String) obj2.get("description");
                     String location = (String) obj2.get("locationName");
-                    sql = "INSERT INTO Events (Name, eventTime, description, location) values('" + eventName + "', '" + eventTime + "', '" + desc + "', '" + location +"')";
+                    //sql = "INSERT INTO Events (Name, eventTime, description, location) values('" + eventName + "', '" + eventTime + "', '" + desc + "', '" + location +"')";
+                    addEvent(obj2);
                 } else if (function.equals("leaveCommunity")) {
-
+                    leaveCommunity(obj2);
                 }
 
             } catch (SocketTimeoutException s) {
@@ -88,48 +86,113 @@ public class Server extends Thread {
         }
     }
 
-    private void addUser(JSONObject obj) {
-        String first = addWcomma((String) obj.get("firstName"));
-        String last = addWcomma((String) obj.get("lastName"));
-        String google = addWcomma((String) obj.get("GoogleID"));
-        String dateCreated = addEnd((String) obj.get("dateCreated"));
+    private void leaveCommunity(JSONObject obj) {
+
+    }
+
+    private void addEvent(JSONObject obj) {
+        String idEvents = "0";
+        String communityID = "0";
+        String name = (String) obj.get("name");
+        String description = (String) obj.get("description");
+        String date = (String) obj.get("date");
+        String time = (String) obj.get("time");
+        String city = (String) obj.get("city");
+        String state = (String) obj.get("state");
+        String address = (String) obj.get("address");
+        String zipcode = (String) obj.get("zipcode");
+        String locationName = (String) obj.get("locationName");
+        String numAttendees = (String) obj.get("numAttendees");
         try {
-            String queryCheck = "SELECT count(*) from Users WHERE GoogleID = '" + obj.get("GoogleID") + "'";
-            System.out.println(queryCheck);
-            rs = stmt.executeQuery(queryCheck);
-            if(rs.next()) {
-                if (rs.getInt(1) == 0) {
-                    String sql = "INSERT INTO Users VALUES('7', " + first + last + google + dateCreated + ")";
-                    System.out.println(sql);
-                    stmt.execute(sql);
-                } else {
-                    
-                }
-            }
+            String sql = "INSERT INTO Communities VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            ps = conn.prepareStatement(sql);
+            ps.setString(10, idEvents);
+            System.out.println(ps);
+            ps.executeUpdate();
+            out.writeUTF("0");
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private String addFirst(String value) {
-        String formatted = "'";
-        formatted += value;
-        formatted += "' ";
-        return formatted;
+    private void addCommunity(JSONObject obj) {
+        String idCommunities = "0";
+        String neighboorhoodID = "0";
+        String category = (String) obj.get("category");
+        String subCategory = (String) obj.get("subCategory");
+        String name = (String) obj.get("name");
+        String description = (String) obj.get("description");
+        String numMembers = "1";
+        String numUpcomgEvents = "0";
+        String dateCreated = (String) obj.get("dateCreated");
+        String Private = (String) obj.get("private");
+        try {
+            queryCheck = "SELECT count(*) from Communities WHERE name = ?";
+            ps = conn.prepareStatement(queryCheck);
+            ps.setString(1, name);
+            System.out.println(ps);
+            rs = ps.executeQuery();
+            if(rs.next()) {
+                if (rs.getInt(1) == 0) {
+                    String sql = "INSERT INTO Communities VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    ps = conn.prepareStatement(sql);
+                    ps.setString(1, idCommunities);
+                    ps.setString(2, neighboorhoodID);
+                    ps.setString(3, category);
+                    ps.setString(4, subCategory);
+                    ps.setString(5, name);
+                    ps.setString(6, description);
+                    ps.setString(7, numMembers);
+                    ps.setString(8, numUpcomgEvents);
+                    ps.setString(9, dateCreated);
+                    ps.setString(10, Private);
+                    System.out.println(ps);
+                    ps.executeUpdate();
+                } else {
+                    out.writeUTF("1");
+                }
+            }
+            out.writeUTF("0");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private String addWcomma(String value) {
-        String formatted = "'";
-        formatted += value;
-        formatted += "', ";
-        return formatted;
-    }
-
-    private String addEnd(String value) {
-        String formatted = "'";
-        formatted += value;
-        formatted += "'";
-        return formatted;
+    private void addUser(JSONObject obj) {
+        String first = (String) obj.get("firstName");
+        String last = (String) obj.get("lastName");
+        String google = (String) obj.get("GoogleID");
+        String dateCreated = (String) obj.get("dateCreated");
+        try {
+            queryCheck = "SELECT count(*) from Users WHERE GoogleID = ?";
+            ps = conn.prepareStatement(queryCheck);
+            ps.setString(1, google);
+            System.out.println(ps);
+            rs = ps.executeQuery();
+            if(rs.next()) {
+                if (rs.getInt(1) == 0) {
+                    String sql = "INSERT INTO Users VALUES('7', ?, ?, ?, ?)";
+                    ps = conn.prepareStatement(sql);
+                    ps.setString(1, first);
+                    ps.setString(2, last);
+                    ps.setString(3, google);
+                    ps.setString(4, dateCreated);
+                    System.out.println(ps);
+                    ps.executeUpdate();
+                } else {
+                    out.writeUTF("1");
+                }
+            }
+            out.writeUTF("0");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
