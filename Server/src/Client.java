e8622e535b9205f64825b1344e46fc83518549a6
trankq <trankq@purdue.edu>
2016-11-07 03:29:10 -0500

    test 123

--- Server/src/Client.java
+++ Server/src/Client.java
@@ -9,7 +9,7 @@ import org.json.simple.JSONObject;
 public class Client {
     public static void main(String[] args) {
         String serverName = "128.211.225.79";
-        //String serverName = "10.186.85.23";
+        //String serverName = "10.186.91.182";
         int port = 3300;
         try {
             System.out.println("Connecting to " + serverName + " on port " + port);
@@ -22,9 +22,16 @@ public class Client {
             JSONObject obj;
             JSONObject obj2;
 
-            /*obj.put("function", "addUser"); obj.put("firstName", "Khanh");
+            /*obj = new JSONObject();
+            obj.put("function", "addUser"); obj.put("firstName", "Khanh");
             obj.put("lastName", "Tran");
             obj.put("googleID", "newcdragon@gmail.com");
+            out.writeUTF(obj.toString());*/
+
+            /*obj = new JSONObject();
+            obj.put("function", "updateInstanceID");
+            obj.put("googleID", "newcdragon@gmail.com");
+            obj.put("instanceID", "ertyui178394iujrhtnbg");
             out.writeUTF(obj.toString());*/
 
             /*obj2.put("function", "addUser"); obj2.put("firstName", "Larry");
@@ -104,7 +111,7 @@ public class Client {
 
             obj = new JSONObject();
             obj.put("function", "genericNotification");
-            obj.put("idUsers", "1"); obj.put("message", "128.211.225.79");
+            obj.put("idUsers", "1"); obj.put("message", "asdf");
             out.writeUTF(obj.toString());
             //System.out.println("Server says " + in.readUTF());
             //System.out.println("Server says " + in.readUTF());
